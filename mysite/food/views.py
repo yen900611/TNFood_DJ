fa375920966191ce4f2b22f71e05485eeee3da60
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-09-19 18:05:41 +0800

    refactor: use for in template to trim code size

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello food!")
-    return render(request, 'food/index.html')
+    return render(request, 'food/index.html', {'place_nums': range(6), }
+
+                  )
