088121f36e6c87ac7b7b1c298becf184a6aeaec0
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-09-19 17:28:50 +0800

    add index hello world

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    path('', index),
+    # path('', index)
 ]
