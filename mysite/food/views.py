11c4a9b7c4c228a871123485d6e673e10680424a
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-09-19 17:44:22 +0800

    add template index

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello food!")
+    # return HttpResponse("Hello food!")
+    return render(request, 'food/index.html')
