c5bd5729ced4a1df764bc8da7a81ebe4ab02d4df
yanshaoting <yen900611@gmail.com>
2022-09-20 21:51:25 +0800

    add Store model

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    store_name = models.CharField(max_length=20, help_text='Enter the name of store')
+    address = models.CharField(max_length=50, null=True)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.store_name
+
