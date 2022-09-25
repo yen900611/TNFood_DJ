55f4ab5976e01a682693a5ae5152f28775853e9f
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-09-25 16:39:51 +0800

    add mimetypes conf in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
