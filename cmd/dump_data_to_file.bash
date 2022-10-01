a66af1631ba25858aad33a4008d231f9cdf3d9d1
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-10-01 22:47:28 +0800

    add admin for Place

--- cmd/dump_data_to_file.bash
+++ cmd/dump_data_to_file.bash
@@ -2,6 +2,6 @@ export PROJECT_HOME=$(pwd)
 cd $PROJECT_HOME/mysite
 python manage.py dumpdata --natural-foreign --natural-primary \
 -e contenttypes -e auth -e admin -e sessions \
---indent 2 > ./db/latest_db.json
-cp ./db/latest_db.json ./db/$(date -I date).json
+--indent 2 -o ./db/latest_db.json
+cp ./db/latest_db.json ./db/$(date -I ).json
 cd $PROJECT_HOME
--- mysite/food/admin.py
+++ mysite/food/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from .models import Photo, Place, Tag, Tag_Management, Device, Device_Management
 
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'web_site', 'pub_date')
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Photo, admin.ModelAdmin)
-admin.site.register(Place, admin.ModelAdmin)
 admin.site.register(Tag, admin.ModelAdmin)
 admin.site.register(Tag_Management, admin.ModelAdmin)
 admin.site.register(Device, admin.ModelAdmin)
