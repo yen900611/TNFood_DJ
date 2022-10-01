b3c16983443d909626f343bfabe8a562275a0655
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-10-01 23:28:28 +0800

    add inline in PlaceAdmin

--- mysite/food/admin.py
+++ mysite/food/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 from .models import Photo, Place, Tag, Tag_Management, Device, Device_Management
 
 
+class TagInlineAdmin(admin.StackedInline):
+    model = Place.tags.through
+
+
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'address', 'web_site', 'pub_date')
-    list_filter = ( 'pub_date','tags')
+    list_filter = ('pub_date', 'tags')
     search_fields = ('name', 'address', 'web_site')
     ordering = ('-id',)
 
+    readonly_fields = ('pub_date',)
+
+    inlines = [TagInlineAdmin, ]
 
 
 admin.site.register(Place, PlaceAdmin)
