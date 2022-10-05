0a39629e71be68b22073da6baa66ae46c16be299
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-10-05 17:20:23 +0800

    refactor api

--- mysite/food/api.py
+++ mysite/food/api.py
@@ -1,22 +1,36 @@
-from typing import List
+import datetime
+from typing import List, Optional
 
-from ninja import NinjaAPI, Schema, File
+from ninja import NinjaAPI, Schema, File, ModelSchema
 from ninja.files import UploadedFile
-from .models import Place, Photo, Tag
+from pydantic import AnyUrl
+
+from .models import Place, Photo, Tag, Device
 
 api = NinjaAPI()
 
 
 class Tags(Schema):
-    name: str
-    style: str
+    display: str
+    value: str
 
 
-class Places(Schema):
-    name: str
-    address: str
-    phone_number: str
-    photos: list
+class DeviceSchema(ModelSchema):
+    class Config:
+        model = Device
+        model_fields = ['name']
+
+
+class PlacesSchema(Schema):
+    name: str = "店家"
+    address: str = "地址"
+    phone_number: str = "電話"
+    photos: Optional[list[str]]
+    web_site: str = ""
+    introduction: str = ""
+    pub_date: datetime.datetime
+    tag: list[Tags]
+    devices = list[DeviceSchema]
 
 
 @api.get("tags", response=List[Tags])
@@ -24,30 +38,34 @@ def tags(request):
     tag = Tag.objects.all()
     return tag
 
-@api.post("tags")
-def add_tag(request, pay_load:Tags):
-    tag = Tag.objects.create(**pay_load.dict())
-    return {"id":tag.id}
 
-@api.get("places")
+@api.post("tags")
+def add_tag(request, pay_load: Tags):
+    tag = Tag.objects.create(**pay_load.dict())
+    return {"id": tag.id}
+
+
+@api.get("places",
+         response=List[PlacesSchema]
+         )
 def places(request):
-    places = Place.objects.all().prefetch_related('photo_set')
+    places = Place.objects.all().prefetch_related('photo_set', 'tag')
     # TODO refactor to Schema
-    result = [{
-        'name': place.name,
-        'address': place.address,
-        'phone_number': place.phone_number,
-        'photo_url': place.photo_set.first().file.url
-    } for place in places]
+
+    result = [PlacesSchema(
+        **place.__dict__,
+        tag=[Tags(display=t.name,value=t.style) for t in place.tag.all()],
+        photos=[photo.file.url for photo in place.photo_set.all()]
+    ) for place in places]
     return result
-@api.get("place")
-def places(request, id):
-    place = Place.objects.get(id=id)
-    photo = place.photo_set.first()
-    result = {
-        'name': place.name,
-        'address': place.address,
-        'phone_number': place.phone_number,
-        'photo_url': photo.file.url
-    }
+
+
+@api.get("place", response=PlacesSchema)
+def get_place(request, id=1):
+    place = Place.objects.prefetch_related('photo_set', 'tag').get(id=id)
+    result = PlacesSchema(
+        **place.__dict__,
+        tag=[Tags(display=t.name,value=t.style) for t in place.tag.all()],
+        photos=[photo.file.url for photo in place.photo_set.all()]
+    )
     return result
--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.urls import path, include
 # from .views import index, place_introduction
 from .api import api
+from .views import place_introduction, index
 
-def index(request):
-    return render(request,'food/index.html')
-def place_introduction(request):
-    return render(request,'food/place_introduction.html')
+# def index(request):
+#     return render(request,'food/index.html')
+# def place_introduction(request):
+#     return render(request,'food/place_introduction.html')
 
 urlpatterns = [
     path('', index, name='food_index'),
