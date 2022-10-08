93ae5102a49cdfc32d9dce965bd1dda6acdbaa4c
yanshaoting <yen900611@gmail.com>
2022-10-08 23:25:19 +0800

    tag db

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=10)
+    value = models.CharField(max_length=30, default="None")
     style = models.CharField(max_length=30, null=True)
 
     def __str__(self):
