dcd651acff7616a802ff277ecc70f34178b79ac5
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-10-10 18:50:30 +0800

    add sentry

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -145,3 +147,22 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5500",
 ]
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+SENTRY_CLIENT_DSN = os.getenv('SENTRY_CLIENT_DSN')
+sentry_sdk.init(
+    dsn=SENTRY_CLIENT_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.01,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
