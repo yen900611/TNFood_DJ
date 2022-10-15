0a2a7adaf1e810fb5e201331272a5352bfdabb41
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-10-15 12:04:04 +0800

    add gzip

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
