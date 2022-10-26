e0b3169e0f2b5f52576a1fb0becfc9a4c7fe5a0f
yanshaoting <yen900611@gmail.com>
2022-10-26 11:57:42 +0800

    debug

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -14,13 +14,14 @@ class Tag(models.Model):
     category = 'C'
     food_style = 'F'
     name = models.CharField(max_length=10)
-    value = models.CharField(max_length=30, default="None",unique=True)
+    value = models.CharField(max_length=30, default="None")
     group = models.CharField(max_length=30, default=food_style, choices=[(vegan_style, 'vegan_style'),
-                                                                (category, 'category'),
-                                                                (food_style, 'food_style'), ])
+                                                                         (category, 'category'),
+                                                                         (food_style, 'food_style'), ])
 
     def __str__(self):
         return self.name
+
 
 class Device(models.Model):
     name = models.CharField(max_length=20)
@@ -35,7 +36,7 @@ class Place(models.Model):
     phone_number = models.CharField(max_length=20, default="No phone number")
     web_site = models.CharField(max_length=200, default="No web site")
     introduction = models.CharField(max_length=100, default="不用問去吃就對了")
-    pub_date = models.DateField('date published', default=datetime.date.today())
+    pub_date = models.DateField('date published', auto_now=True)
     tag = models.ManyToManyField(Tag, blank=True)
     devices = models.ManyToManyField(Device, blank=True)
 
